06d1959a62cc13e6ea878324fb7c92bd2b97b8c4
Joel <joelwrichardson01@gmail.com>
2020-10-16 21:35:14 +1000

    functsthing

--- types.h
+++ types.h
@@ -1,7 +1,9 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-struct nvec {
+#define color int
+
+struct vec {
     int dimension;
     double *elements;
 };
@@ -11,11 +13,42 @@ enum solid_op {
 };
 
 
+struct ray 
+{
+    vec pos;
+    vec dir;
+};
+
+struct camera
+{
+    vec pos;
+    vec rot;
+    int dims;
+};
+
+struct pixel_info
+{
+    int flags; /* 0 - no collide */
+    color col;
+    int iterations;
+    double travel_dist;
+    double scene_dist;
+};
+
+
+
+struct object
+{
+    solid sol;
+    color (*col)(struct ray);
+};
+
 struct solid {
     enum solid_op op;
     double rotation;
     double scale;
-    struct nvec distfunc;
+    double (*dist)(struct vec *);
 };
+
 
 #endif
