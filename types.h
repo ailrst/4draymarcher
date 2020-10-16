abe44ba5f6a87965040f609abd17fe1b2331f9bf
alistair <alistair@topost.net>
2020-10-16 19:11:57 +1000

    files

---
+++ types.h
@@ -0,0 +1,21 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+struct nvec {
+    int dimension;
+    double *elements;
+};
+
+enum solid_op {
+    ADD
+};
+
+
+struct solid {
+    enum solid_op op;
+    double rotation;
+    double scale;
+    struct nvec distfunc;
+};
+
+#endif
